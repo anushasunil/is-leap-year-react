056b2bfe43e0bb6718c7690b3094f31fd8201dba
Anusha <90134948+anushasunil@users.noreply.github.com>
2021-10-04 05:22:43 +0530

    fix: validation function

--- src/App.js
+++ src/App.js
@@ -6,23 +6,34 @@ export default function App() {
   var [year, setYear] = useState("");
 
   function isLeapYear() {
-    if (Number(year) % 400 === 0) {
-      return true;
-    }
-    if (Number(year) % 100 === 0) {
-      return false;
-    }
-    if (Number(year) % 4 === 0) {
-      return true;
+    if (year > 0) {
+      if (Number(year) % 400 === 0) {
+        return 1;
+      }
+      if (Number(year) % 100 === 0) {
+        return 2;
+      }
+      if (Number(year) % 4 === 0) {
+        return 1;
+      }
+    } else {
+      return 3;
     }
   }
 
   function clickHandler() {
     var flag = isLeapYear();
-    if (flag === true) {
-      setStatement(year + " is a leap year");
-    } else {
-      setStatement(year + " is not a leap year");
+    switch (flag) {
+      case 1: {
+        setStatement(year + " is a leap year.");
+        break;
+      }
+      case 2: {
+        setStatement(year + " is not a leap year.");
+        break;
+      }
+      default:
+        setStatement("please enter a valid input.");
     }
   }
 
