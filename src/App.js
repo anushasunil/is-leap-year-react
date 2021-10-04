af5581f94d77111faa35cf93d06a4cabc9873d0c
Anusha <90134948+anushasunil@users.noreply.github.com>
2021-10-04 23:42:47 +0530

    fix: check function

--- src/App.js
+++ src/App.js
@@ -6,30 +6,32 @@ export default function App() {
   var [year, setYear] = useState("");
 
   function isLeapYear() {
+    var flag = 0;
     if (year > 0) {
       if (Number(year) % 400 === 0) {
         return 1;
       }
       if (Number(year) % 100 === 0) {
-        return 2;
+        return 0;
       }
       if (Number(year) % 4 === 0) {
         return 1;
       }
     } else {
-      return 3;
+      return 2;
     }
+    return flag;
   }
 
   function clickHandler() {
     var flag = isLeapYear();
     switch (flag) {
-      case 1: {
-        setStatement(year + " is a leap year.");
+      case 0: {
+        setStatement(year + " is not a leap year.");
         break;
       }
-      case 2: {
-        setStatement(year + " is not a leap year.");
+      case 1: {
+        setStatement(year + " is a leap year.");
         break;
       }
       default:
